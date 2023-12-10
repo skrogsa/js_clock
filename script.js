85ef18baa2b222e2052d6e686583f37c67273804
skrogsa <siljekrogsater@gmail.com>
2023-12-10 22:20:07 +0100

    added different messages and different times

--- script.js
+++ script.js
@@ -1,38 +1,50 @@
-console.log("Hello world");
+const theName = "Silje"
 
-/*
 function helloTime() {
     let d = new Date();
     let hours = d.getHours();
     let minutes = d.getMinutes();
-    if (hours >= 4 && minutes >= 0) {
+    if ((hours >= 4 && minutes >= 0) && (hours <= 12 && minutes <= 59)) {
         console.log("God morgen!");
-    }  else if (hours >= 12 && minutes >= 0) {
-        console.log("God dag!");
-    }  else if (hours >= 15 && minutes >= 0) {
-        console.log("God ettermiddag!");
-    }  else if (hours >= 18 && minutes >= 0) {
-        console.log("God kveld!");
-    }  else if (hours >= 0 && minutes >= 0) {
-        console.log("God natt!");
-    }
-  }
-*/
-
-const theName = "Silje"
-
-function goodDay() {
-    let d = new Date();
-    let hours = d.getHours();
-    let minutes = d.getMinutes();
-    if (hours >= 13 && minutes >= 0) {
-        console.log("God morgen!");
-
         const display = document.getElementById("display");
         display.textContent = `God morgen, ${theName}!`;
-    }  else {
+    }  else if ((hours >= 13 && minutes >= 0) && (hours <= 14 && minutes <= 59)) {
         console.log("God dag!");
-}
-}
+        const display = document.getElementById("display");
+        display.textContent = `God dag, ${theName}!`;
+    }  else if ((hours >= 15 && minutes >= 0) && (hours <= 17 && minutes <= 59)) {
+        console.log("God ettermiddag!");
+        const display = document.getElementById("display");
+        display.textContent = `God ettermiddag, ${theName}!`;
+    }  else if ((hours >= 18 && minutes >= 0) && (hours <= 23 && minutes <= 59)) {
+        console.log("God kveld!");
+        const display = document.getElementById("display");
+        display.textContent = `God kveld, ${theName}!`;
+    }  else if ((hours >=0 && minutes >= 0)  && (hours <= 3 && minutes <= 59)) {
+        console.log("God natt!");
+        const display = document.getElementById("display");
+        display.textContent = `God natt, ${theName}!`;
+    }
+  }
 
-goodDay()
+
+  helloTime()
+
+
+// const theName = "Silje"
+
+// function goodDay() {
+//     let d = new Date();
+//     let hours = d.getHours();
+//     let minutes = d.getMinutes();
+//     if (hours >= 13 && minutes >= 0) {
+//         console.log("God morgen!");
+
+//         const display = document.getElementById("display");
+//         display.textContent = `God morgen, ${theName}!`;
+//     }  else {
+//         console.log("God dag!");
+// }
+// }
+
+// goodDay()
